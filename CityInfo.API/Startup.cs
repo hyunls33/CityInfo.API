b2a7409ca556ac0493321ada0930dbcef22dd622
hyunls33 <36907266+hyunls33@users.noreply.github.com>
2018-10-16 17:22:42 +0900

    Revert "Update CityInfo.API/Startup.cs"
    This reverts commit 76c791d0b8f62279accc41753ca47d2290eac1e8.
    
    # Conflicts:
    #	CityInfo.API/Startup.cs

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -28,15 +28,7 @@ namespace CityInfo.API
 
             app.Run(async (context) =>
             {
-<<<<<<< HEAD
-<<<<<<< HEAD
-                //마스터 코드 추가했습니다~!!
-=======
-                //롤백 테스트요!
->>>>>>> parent of eef0354... 마스터 체인지
-=======
                 //Branch test code insert!!!!
->>>>>>> test1
                 await context.Response.WriteAsync("Merge Test");
             });
         }
