854357bf850cbf70b016cf28853f1fbadbd40819
hyunls33 <36907266+hyunls33@users.noreply.github.com>
2018-10-16 17:20:15 +0900

    Update CityInfo.API/Startup.cs

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -35,6 +35,7 @@ namespace CityInfo.API
                 //롤백 테스트요!
 >>>>>>> parent of eef0354... 마스터 체인지
 =======
+                //Branch test code insert!!!!
 >>>>>>> test1
                 await context.Response.WriteAsync("Merge Test");
             });
