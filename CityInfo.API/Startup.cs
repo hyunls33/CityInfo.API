783b29a5b46ec7e88585951e6e35807f5b0a7976
hyunls33 <36907266+hyunls33@users.noreply.github.com>
2018-10-16 12:57:42 +0900

    Update CityInfo.API/Startup.cs

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -15,6 +15,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,7 @@ namespace CityInfo.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Master Branch");
             });
         }
     }
